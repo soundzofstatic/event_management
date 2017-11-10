4b3b8c425b1d3109932d7ccdd21f571312f12e5a
Daniel Paz <Daniel@Daniels-MacBook-Pro.local>
2017-11-09 22:18:43 -0600

    New methods for Venues class including getters for fields, selectRecord and checkDuplicateRecord. Renames writeToFile method to createRecord

--- src/Venue.java
+++ src/Venue.java
@@ -1,10 +1,11 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class Venue {
 
-    private int id;
+    private String id;
     private String name;
     private String streetAddress;
     private String locality;
@@ -14,7 +15,29 @@ public class Venue {
     private int capacity;
     private int[] typeOfSeating;
 
-    public static final String DEFAULT_VENUE_FILE = "src/venues.csv";
+    private static final String DEFAULT_VENUE_FILE = "src/venues.csv";
+
+    /**
+     * Venue constructor that takes a Venue UUID string and name and implicitly reads data from venue file and set object fields
+     *
+     * @param id
+     * @param name
+     */
+    public Venue(String id, String name){
+
+        this.id = id;
+        this.name = name;
+
+        // run selectRecord(); to pull data from database
+        try {
+            selectRecord();
+
+        } catch (FileNotFoundException e1) {   // Catch block
+
+            System.out.println("File not Found");
+        }
+
+    }
 
     /**
      * Venue constructor used to accept client code definitions for a venue and implicitly writes data to a file
@@ -43,34 +66,248 @@ public class Venue {
 
         // Write to File
         try {
-            writeToFile();
+            createRecord();
         } catch (FileNotFoundException e1) {
             // Catch block
             System.out.println("File not Found");
         }
 
+    }
+
+    public String getId()
+    {
+
+        return this.id;
+
+    }
+
+    public String getName()
+    {
+
+        return this.name;
+
+    }
+
+    public String getStreetAddress()
+    {
+
+        return this.streetAddress;
+
+    }
+
+    public String getLocality()
+    {
+
+        return this.locality;
+
+    }
+
+    public String getRegion()
+    {
+
+        return this.region;
+
+    }
+
+    public String getCountry()
+    {
+
+        return this.country;
+
+    }
+
+    public String getZipCode()
+    {
+
+        return this.zipCode;
+
+    }
+
+    public int getCapacity()
+    {
+
+        return this.capacity;
+
+    }
+
+    public int[] getTypeOfSeating()
+    {
+
+        return this.typeOfSeating;
 
     }
 
     /**
-     * Private method used to write to object fields to a venues.csv file
+     * Private method used to write object fields to a venues.csv file.
      *
      * @throws FileNotFoundException
      */
-    private void writeToFile() throws FileNotFoundException
+    private void createRecord() throws FileNotFoundException
     {
+        // Generate a UUID ID
+        UUID uuid = UUID.randomUUID();
 
-        String record = name + ", " + streetAddress + ", " + locality + ", " + region + ", " + country + ", " + zipCode + ", " + capacity + ", " + Arrays.toString(typeOfSeating);
+        // Set the id for the object
+        this.id = uuid.toString();
+
+        // Create the String that represents a record
+        String record = "\"" + id + "\",\"" + name + "\",\"" + streetAddress + "\",\"" + locality + "\",\"" + region + "\",\"" + country + "\",\"" + zipCode + "\",\"" + capacity + "\",\"" + Arrays.toString(typeOfSeating) + "\"";
 
         // Instantiate a File output stream and set the pointer to the end of file in order to append
         // Creates file if it does not exist
         FileOutputStream file_output_stream = new FileOutputStream(new File(this.DEFAULT_VENUE_FILE), true);
 
-        // Create a PrintStream object that will be used to output the file
-        PrintStream output = new PrintStream(file_output_stream);
+        // In order to avoid duplicate records, use id and name as keys and check if they exist in database
+        if(!this.checkDuplicateRecord()) {
 
-        // Print to file
-        output.println(record);
+            // Create a PrintStream object that will be used to output the file
+            PrintStream output = new PrintStream(file_output_stream);
+
+            // Print to file
+            output.println(record);
+
+        } else {
+
+            // todo - Does file_output_stream need to be closed in order to conserve resources?
+
+        }
+
+    }
+
+    /**
+     * Private method used to select a record from the venues.csv file and write the value to the object fields
+     *
+     * @throws FileNotFoundException
+     */
+    private void selectRecord() throws FileNotFoundException
+    {
+
+        // Get input from the File
+        Scanner fileInput = new Scanner(new File(this.DEFAULT_VENUE_FILE));
+
+        while(fileInput.hasNextLine()){
+
+            // read record line
+            String record = fileInput.nextLine();
+
+            // Before exploding, replace problematic characters
+            String cleanRecord1 = record.replace("\",", "|");
+            String cleanRecord2 = cleanRecord1.replace("\"", "");
+
+            // Explode the Record into individual elements
+            String[] recordArray = cleanRecord2.split("\\|");
+
+            // Skip records that don't have matching id
+            if(this.id != null && !recordArray[0].toLowerCase().equals(this.id.toLowerCase())) {
+
+               continue;
+
+            }
+
+            // Skip records that don't have matching name
+            if(this.name != null && !recordArray[1].toLowerCase().equals(this.name.toLowerCase())) {
+
+                continue;
+
+            }
+
+            System.out.println(Arrays.toString(recordArray));
+
+            // Iterate over elements and map to object fields
+            for(int i = 0; i < recordArray.length; i++){
+                switch(i){
+                    case 0:
+                        this.id = recordArray[i];
+                        break;
+                    case 1:
+                        this.name = recordArray[i];
+                        break;
+                    case 2:
+                        this.streetAddress = recordArray[i];
+                        break;
+                    case 3:
+                        this.locality = recordArray[i];
+                        break;
+                    case 4:
+                        this.region = recordArray[i];
+                        break;
+                    case 5:
+                        this.country = recordArray[i];
+                        break;
+                    case 6:
+                        this.zipCode = recordArray[i];
+                        break;
+                    case 7:
+                        System.out.println(recordArray[i]);
+                        this.capacity = Integer.parseInt(recordArray[i]);
+                        break;
+                    case 8:
+                        System.out.println(recordArray[i]);
+                        int[] seatMap = {1, 2, 3};
+                        this.typeOfSeating = seatMap;//recordArray[i]; // todo - need algorithm to convert string representation of array (or JSON array), potentially multi-dim array back into an JAVA array
+                        break;
+                }
+            }
+
+            // Don't continue on, break while loop
+            break;
+        }
+
+    }
+
+    /**
+     * Helper method used to check for duplicate records in our "database" file.
+     * Uses id and name as unique keys
+     *
+     * @return boolean
+     * @throws FileNotFoundException
+     */
+    private boolean checkDuplicateRecord() throws FileNotFoundException
+    {
+
+        // Get input from the File
+        Scanner fileInput = new Scanner(new File(this.DEFAULT_VENUE_FILE));
+
+        while(fileInput.hasNextLine()){
+
+            // read record line
+            String record = fileInput.nextLine();
+
+            // Explode the Record into individual elements
+            String[] recordArray = record.split(",");
+
+            // Iterate over the first two elements of the array (expected to be id and name, respectively)
+            for(int i=0; i < 2; i++){
+
+                if(i == 0){// if id
+
+                    if(recordArray[i].toLowerCase().substring(1, (recordArray[i].length() - 1)).equals(this.id.toLowerCase())){
+
+                        System.out.println("Duplicate Venue ID.");
+
+                        return true;
+
+                    }
+
+                }
+
+                if(i == 1){//if name
+
+                    if(recordArray[i].toLowerCase().substring(1, (recordArray[i].length() - 1)).equals(this.name.toLowerCase())){
+
+                        System.out.println("Duplicate Venue Name.");
+
+                        return true;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return false;
 
     }
 
