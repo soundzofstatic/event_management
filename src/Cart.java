14682fd19447db79f3410380f657c056e36c733f
Daniel Paz <dpaz314@gmail.com>
2017-12-08 19:34:03 -0600

    Removes Cart.java from root and moves contents of it into src/Cart.java

--- src/Cart.java
+++ src/Cart.java
@@ -22,118 +22,118 @@ public class Cart{
 	private double total = 0.0;
 	private int tixQuantity = 0;
 	private Date date = new Date();
-	
+
 	private static final String DEFAULT_TRANSACTIONS_FILE = "src/transactions.csv";
 
 	/**
-     * Cart constructor that implicitly sets the Cart ID
-     *
-     */
+	 * Cart constructor that implicitly sets the Cart ID
+	 *
+	 */
 	public Cart(){
 
 		this.tixList = new ArrayList<Ticket>();
 		UUID uuid = UUID.randomUUID();
 
-        // Set the id for the object
-        this.id = uuid.toString();
+		// Set the id for the object
+		this.id = uuid.toString();
 
 	}
 
-	
+
 	/**
-     * Public Getter method that returns the value of this.tixList
-     *
-     * @return ArrayList
-     */
+	 * Public Getter method that returns the value of this.tixList
+	 *
+	 * @return ArrayList
+	 */
 	public ArrayList<Ticket> getTixList(){
 
 		return this.tixList;
 
 	}
-	
-	
+
+
 	/**
-     * Public Getter method that returns the value of this.subtotal
-     *
-     * @return 
-     */
+	 * Public Getter method that returns the value of this.subtotal
+	 *
+	 * @return
+	 */
 	public double getSubtotal(){
 		return this.subtotal;
 	}
-	
-	
+
+
 	/**
-     * Public Getter method that returns the value of this.tax
-     *
-     * @return
-     */
+	 * Public Getter method that returns the value of this.tax
+	 *
+	 * @return
+	 */
 	public double getTax(){
 		return this.tax;
 	}
-	
-	
+
+
 	/**
-     * Public Getter method that returns the value of this.total
-     *
-     * @return
-     */
+	 * Public Getter method that returns the value of this.total
+	 *
+	 * @return
+	 */
 	public double getTotal(){
 		return this.total;
 	}
-	
-	
+
+
 	/**
-     * Public Getter method that returns the value of this.tixQuantity
-     *
-     * @return
-     */
+	 * Public Getter method that returns the value of this.tixQuantity
+	 *
+	 * @return
+	 */
 	public int getTixQuantity(){
 		return this.tixQuantity;
 	}
-	
+
 
 	/**
-     * Public Setter method that sets the value of this.tixList
-     *
-     * @param newTixList
-     */
+	 * Public Setter method that sets the value of this.tixList
+	 *
+	 * @param newTixList
+	 */
 	public void setTixList(ArrayList<Ticket> newTixList){
 
 		this.tixList = newTixList;
 
 	}
 
-	
+
 	/**
-     * Public method that adds a new ticket to this.tixList, recalculates the subtotal, tax, and total of the cart, and returns a boolean denoting whether the addition was successful or not.
-     *
-     * @param newTicket
-	 * @return 
-     */
+	 * Public method that adds a new ticket to this.tixList, recalculates the subtotal, tax, and total of the cart, and returns a boolean denoting whether the addition was successful or not.
+	 *
+	 * @param newTicket
+	 * @return
+	 */
 	public boolean add(Ticket newTicket){
 		boolean addSuccess = false;
 		Scanner console = new Scanner(System.in);
-		
-		while (int i = 0; i < tixList.length(); i++){
+
+		for(int i = 0; i < this.tixList.size(); i++){
 			if (this.tixList.get(i).getId() == newTicket.getId()){
-			return addSuccess;
+				return addSuccess;
 			}
 		}
-		
+
 		this.tixList.add(newTicket);
 		calculateTotal(newTicket.getPrice(), true);
-		this.tixQuantity++
+		this.tixQuantity++;
 		addSuccess = true;
 		return addSuccess;
 	}
 
-	
+
 	/**
-     * Public method that removes a ticket by ID from this.tixList (if it exists within the list), recalculates the subtotal, tax, and total of the cart, and returns a boolean denoting whether the removal was successful or not.
-     *
-     * @param ticketId
-	 * @return 
-     */
+	 * Public method that removes a ticket by ID from this.tixList (if it exists within the list), recalculates the subtotal, tax, and total of the cart, and returns a boolean denoting whether the removal was successful or not.
+	 *
+	 * @param ticketId
+	 * @return
+	 */
 	public boolean remove(String ticketId){
 
 		Scanner console = new Scanner(System.in);
@@ -142,149 +142,150 @@ public class Cart{
 		if (ticketId == null){
 			return removeSuccess;
 		}
-		
-		for (int i = 0; i < tixList.length(); i++){
+
+		for (int i = 0; i < tixList.size(); i++){
 			if (this.tixList.get(i).getId().toLowerCase() == ticketId.toLowerCase()){
-				calculateTotal(this.tixList.get(ticketIndex).getPrice(), false)
-				this.tixList.remove(ticketIndex);
-				this.tixQuantity--
+				calculateTotal(this.tixList.get(i).getPrice(), false);
+				this.tixList.remove(i);
+				this.tixQuantity--;
 				removeSuccess = true;
 				return removeSuccess;
 			}
 		}
-		
+
 		return removeSuccess;
 	}
-	
+
 
 	/**
-     * @return String
-     */
+	 * @return String
+	 */
 	public String toString(){
 		String cartString = "Tickets in Cart: \n";
-		for (int i = 0; i < this.tixList.length(); i++){
+		for (int i = 0; i < this.tixList.size(); i++){
 			cartString += (this.tixList.get(i).getId() + "\n");
 			this.subtotal += (this.tixList.get(i).getPrice());
 		}
-		cartString += ((20 * "-") + "\nSubtotal: $" + String.format("%.2f", this.subtotal)) +
-				("\n7% Tax: $" + String.format("%.2f", this.tax)) +
-				("\nTotal: $" + String.format("%.2f", this.total));
+		cartString += "********************\n";
+		cartString += "Subtotal: $" + String.format("%.2f", this.subtotal);
+		cartString +=  "\n7% Tax: $" + String.format("%.2f", tax);
+		cartString += "\nTotal: $" + String.format("%.2f", total);
 		return cartString;
 	}
 
 
 	/**
-     * Public method that checks the cart out, adding the cart's data to the transactions.csv file and updating the status of the tickets to sold. Returns a boolean denoting if the checkout was successful or not.
-     * 
-	 * @return 
-     */
+	 * Public method that checks the cart out, adding the cart's data to the transactions.csv file and updating the status of the tickets to sold. Returns a boolean denoting if the checkout was successful or not.
+	 *
+	 * @return
+	 */
 	public boolean checkout(){
 		boolean checkoutSuccess = false;
-		
+
 		try {
 			createRecord();
 		} catch (FileNotFoundException e1) {
 			System.out.print("File not found. Checkout canceled.");
 			return checkoutSuccess;
 		}
-		
-		for (int i = 0; i < this.tixList.length(); i++){
-			this.tixlist.get(i).setPurchased(true);
+
+		for (int i = 0; i < this.tixList.size(); i++){
+			this.tixList.get(i).setPurchased(true);
 		}
-		
+
 		checkoutSuccess = true;
 		return checkoutSuccess;
-		
+
 	}
-	
-	
+
+
 	/**
-     * Helper method that recalculates the subtotal, tax amount, and total price of the cart's contents, modifying their respective variables with the new amounts. Can both add to and subtract from the subtotal, based on a boolean.
-     * 
+	 * Helper method that recalculates the subtotal, tax amount, and total price of the cart's contents, modifying their respective variables with the new amounts. Can both add to and subtract from the subtotal, based on a boolean.
+	 *
 	 * @param amountChanged
 	 * @param isAddition
-     */
+	 */
 	private void calculateTotal(double amountChanged, boolean isAddition){
-		
+
 		if(isAddition){
 			this.subtotal += amountChanged;
 		}
 		else{
 			this.subtotal -= amountChanged;
 		}
-		
+
 		this.tax = this.subtotal * this.taxRate;
 		this.total = this.subtotal + this.tax;
-		
+
 	}
-	
-	
+
+
 	/**
-     * Private method used to write object fields to a transactions.csv file.
-     *
-     * @throws FileNotFoundException
-     */
+	 * Private method used to write object fields to a transactions.csv file.
+	 *
+	 * @throws FileNotFoundException
+	 */
 	private void createRecord() throws FileNotFoundException
-    {
-        String record = "\"" + this.id + "\",\"" + this.tixList.length + "\",\"" + String.format("%.2f", this.subtotal)) + "\",\"" + String.format("%.2f", (this.subtotal*.07)) + "\",\"" + convertEpochToDatestamp(this.date.getTime()) + "\",\"" + this.date.getTime() + "\"";
+	{
+		String record = "\"" + this.id + "\",\"" + this.tixList.size() + "\",\"" + String.format("%.2f", this.subtotal)) + "\",\"" + String.format("%.2f", (this.subtotal*.07)) + "\",\"" + Utility.convertEpochToDatestamp(this.date.getTime()) + "\",\"" + this.date.getTime() + "\"";
 
-        FileOutputStream file_output_stream = new FileOutputStream(new File(this.DEFAULT_TRANSACTIONS_FILE), true);
+		FileOutputStream file_output_stream = new FileOutputStream(new File(this.DEFAULT_TRANSACTIONS_FILE), true);
 
-        if(!this.checkDuplicateRecord()) {
+		if(!this.checkDuplicateRecord()) {
 
-            PrintStream output = new PrintStream(file_output_stream);
+			PrintStream output = new PrintStream(file_output_stream);
 
-            output.println(record);
+			output.println(record);
 
-        } else {
+		} else {
 
-            // todo - Does file_output_stream need to be closed in order to conserve resources?
+			// todo - Does file_output_stream need to be closed in order to conserve resources?
 
-        }
+		}
 
-    }
-	
-	
+	}
+
+
 	/**
-     * Helper method used to check for duplicate records in our "database" file.
-     * Uses cart id as unique key
-     *
-     * @return
-     * @throws FileNotFoundException
-     */
+	 * Helper method used to check for duplicate records in our "database" file.
+	 * Uses cart id as unique key
+	 *
+	 * @return
+	 * @throws FileNotFoundException
+	 */
 	private boolean checkDuplicateRecord() throws FileNotFoundException
-    {
+	{
 
-        // Get input from the File
-        Scanner fileInput = new Scanner(new File(this.DEFAULT_TRANSACTIONS_FILE));
+		// Get input from the File
+		Scanner fileInput = new Scanner(new File(this.DEFAULT_TRANSACTIONS_FILE));
 
-        while(fileInput.hasNextLine()){
+		while(fileInput.hasNextLine()){
 
-            // read record line
-            String record = fileInput.nextLine();
+			// read record line
+			String record = fileInput.nextLine();
 
-            // Explode the Record into individual elements
-            String[] recordArray = record.split(",");
+			// Explode the Record into individual elements
+			String[] recordArray = record.split(",");
 
-            // Iterate over the first element of the array (expected to be id)
-            for(int i=0; i < 1; i++){
+			// Iterate over the first element of the array (expected to be id)
+			for(int i=0; i < 1; i++){
 
-                if(i == 0){// if id
+				if(i == 0){// if id
 
-                    if(recordArray[i].toLowerCase().substring(1, (recordArray[i].length() - 1)).equals(this.id.toLowerCase())){
+					if(recordArray[i].toLowerCase().substring(1, (recordArray[i].length() - 1)).equals(this.id.toLowerCase())){
 
-                        System.out.println("Duplicate Transaction ID.");
+						System.out.println("Duplicate Transaction ID.");
 
-                        return true;
+						return true;
 
-                    }
+					}
 
-                }
-            }
+				}
+			}
 
-        }
+		}
 
-        return false;
+		return false;
 
-    }
+	}
 }
